0664b6ea34cd7918471bc780422126b00970b708
Austin Wu <austinwu@Austins-Mac-mini.local>
2022-06-02 22:08:16 -0400

    pushing testing.js to troubleshoot api access

--- assets/testing.js
+++ assets/testing.js
@@ -1,5 +1,4 @@
 // The basic elements of an API call are as follows:
-
 // https://zenquotes.io/api/[mode]/[key]?option1=value&option2=value
 // where:
 
@@ -11,17 +10,24 @@
 // Alternative GET request formats are also available for advanced users:
 // Example: https://zenquotes.io?api=[mode]&key=[your_key]&option1=value&option2=value
 
+// const key = "mFIaFdiyr1io8MpoF3iFJ4zEZLnXVS0m";
+var giphyUrl = "api.giphy.com/v1/gifs/trending/";
+
+var fetchButton = $('#testbutton');
+
 function getApi() {
   // fetch request gets a list of all the repos for the node.js organization
   var requestUrl = 'https://zenquotes.io/api/';
+  let currentCall = requestUrl.concat('quotes/');
+  // let key = prompt("Giphy API key?");
 
-  fetch(requestUrl)
-    .then(function(response) {
+  fetch(requestUrl + "today")
+    .then(function (response) {
       return response.json();
     })
     .then(function(data) {
-        console.log('sdfs');
+      console.log(data);
     });
 }
-
-fetchButton.addEventListener('click', getApi);
+getApi();
+fetchButton.on('click', getApi);
