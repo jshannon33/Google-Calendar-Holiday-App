fca23c17f459edb759f42973ad158c6485ddb56c
Austin Wu <austinwu@Austins-Mac-mini.local>
2022-06-09 22:26:42 -0400

    uploading files for google api and holiday api functionality

---
+++ assets/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    // giphyKey: 'mFIaFdiyr1io8MpoF3iFJ4zEZLnXVS0m',
+};
+
+// Nager Holiday API url
+const HOLIDAY_KEY = 'https://date.nager.at/api/v3/NextPublicHolidays/';
+
+// Google API access info
+const CLIENT_ID = '28605827298-tl4218sn1lc3eie3e9bm6ab7vgrb02gb.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyC1Csyczu-tD3j17uP1qWT7BFEFW-Kzhwc';
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/calendar';
